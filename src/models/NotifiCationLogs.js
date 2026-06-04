9d706090cacfce86c820035d6ff5f0b58bafda1f
tithipanchal <tithipanchal1504@gmail.com>
2026-06-03 14:39:35 -1000

    expense_tracker

--- src/models/NotifiCationLogs.js
+++ src/models/NotifiCationLogs.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 const NotificationLogSchema = new Schema({
     
+    
     userId:{
         type:Schema.Types.ObjectId,
         ref:'User',
